bd92cbcd7863a5c38f834ba4d35d386ca4afa3e0
Lucio Paiva <luciopaiva@gmail.com>
2022-03-17 12:10:37 +0000

    Use openssl cli

--- dtls-test.js
+++ dtls-test.js
@@ -1,41 +1,70 @@
 
-const udp = require("dgram");
-const dtls = require("@nodertc/dtls");
-
-const PORT = 12345;
+const { spawn } = require('child_process');
+const chalk = require("chalk");
+const PORT = 44330;
+const COMMON_SERVER_OPTIONS = `s_server -key private-key.pem -cert public-cert.pem -accept ${PORT} -quiet`.split(/ /);
+const COMMON_CLIENT_OPTIONS = `s_client -4 -connect localhost:${PORT} -quiet`.split(/ /);
+const OPENSSL_FLAVORS = {
+    // the homebrew version right now is `LibreSSL 3.4.3` and enables DTLS v1.2
+    homebrew: {
+        bin: "/usr/local/opt/libressl/bin/openssl",
+        serverOptions: COMMON_SERVER_OPTIONS.concat("-dtls1_2"),
+        clientOptions: COMMON_CLIENT_OPTIONS.concat("-dtls1_2"),
+    },
+    // this is the system default, which on macOS 10.15.7 is `LibreSSL 2.8.3`
+    default: {
+        bin: "openssl",
+        serverOptions: COMMON_SERVER_OPTIONS.concat("-dtls1"),
+        clientOptions: COMMON_CLIENT_OPTIONS.concat("-dtls1"),
+    }
+}
+const OPENSSL = OPENSSL_FLAVORS.homebrew;
+const OPENSSL_BIN = OPENSSL.bin;
+const OPENSSL_SERVER_OPTIONS = OPENSSL.serverOptions;
+const OPENSSL_CLIENT_OPTIONS = OPENSSL.clientOptions;
 
 function startServer() {
     return new Promise(resolve => {
-        const serverSocket = udp.createSocket("udp4");
-        serverSocket.bind(PORT, () => resolve());
+        const server = spawn(OPENSSL_BIN, OPENSSL_SERVER_OPTIONS);
 
-        const server = dtls.createServer({ socket: serverSocket });
-        server.on("error", error => console.error(error));
-        server.on("data", data => {
-            console.info(`[server] received: ${data.toString("utf8")}`);
+        server.on("spawn", () => {
+            console.info("Server started.");
+            resolve();
         });
-        server.on("connect", () => {
-            console.info(`[server] client connected`);
-            server.write("Hello", () => server.close());
+
+        server.stdout.on('data', (data) => {
+            console.log(chalk.blue(`[server] ${data}`));
         });
-        server.on("close", () => console.info(`[server] closed`));
+
+        server.stderr.on('data', (data) => {
+            console.error(chalk.red(`[server] ${data}`));
+        });
+
+        server.on('close', (code) => {
+            console.log(`child process exited with code ${code}`);
+        });
     });
-
 }
 
 function startClient() {
-    const socket = dtls.connect({
-        type: "udp4",
-        remotePort: PORT,
-        remoteAddress: "127.0.0.1",
+    const client = spawn(OPENSSL_BIN, OPENSSL_CLIENT_OPTIONS);
+
+    client.on("spawn", () => {
+        console.info("Client started");
+        client.stdin.write("hello\n");
     });
 
-    socket.on("error", error => console.error(error));
-    socket.on("data", data => {
-        console.info(`[client] received: ${data.toString("utf8")}`);
-        socket.close();
+    client.stdout.on('data', (data) => {
+        console.log(chalk.blue(`[client] ${data}`));
     });
-    socket.once("connect", () => socket.write("Hello world"));
+
+    client.stderr.on('data', (data) => {
+        console.error(chalk.red(`[client] ${data}`));
+    });
+
+    client.on('close', (code) => {
+        console.log(`child process exited with code ${code}`);
+    });
 }
 
 startServer().then(() => startClient());
